6ecf46cd2646e9086dff80b8a436e930ad8a8ced
Evan Turro <evan.turro@gmail.com>
2021-03-18 23:51:12 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "1.15.0"
 
-  bucket = "EVANTURRO-TF-CHIP-BUCKET"
   bucket_prefix = var.prefix
 
   acl    = "private"
