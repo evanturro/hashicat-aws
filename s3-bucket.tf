364abd32339a76045f9a8c41c714096ec25fd97e
Evan Turro <evan.turro@gmail.com>
2021-03-18 23:57:41 +0000

    bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "1.15.0"
 
-  bucket_prefix = var.prefix
+  bucket_prefix = "${var.prefix}"
 
   acl    = "private"
 
